852cbd5693b7fd0cfc0bbb8f498070bf9da40612
princesmith32 <princesmith@funmail.site>
2025-05-31 10:59:24 +0530

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -23,3 +23,20 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
+
+# Add custom CSS
+html_css_files = [
+    'custom.css',
+]
+
+# HTML theme options
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+}
